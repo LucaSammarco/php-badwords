010c853d0da16dd152f5ba517883cccc3fec2522
Luca Sammarco <luca.sammarco7@gmail.com>
2024-06-25 16:45:21 +0200

    set up

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+
+// $name = $_GET["firstname"];
+// $age = "34";
+
+$testo = "Lfddsadsadsdsadas|Dsdasorem"
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <h1>Titolo</h1>
+    <p> <?php echo $testo  ?>
+        <?php echo strlen($testo)   ?>
+    </p>
+
+
+
+    <!-- <form action="./index.php" method="GET">
+        <input type="text" name="firstname" id="first-name">
+        <button type="submit">Premi</button>
+    </form> -->
+
+</body>
+
+</html>
